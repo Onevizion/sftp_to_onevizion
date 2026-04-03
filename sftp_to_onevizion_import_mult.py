669a3a79f2387171e0c704ad8a0955e539926eba
petteway <spetteway@onevizion.com>
2026-04-03 10:59:27 -0400

    change chdir

--- sftp_to_onevizion_import_mult.py
+++ sftp_to_onevizion_import_mult.py
@@ -213,8 +213,8 @@ for row in Req.jsonData:
 	)
 
 	# get complete list of files in directory
-	with sftp.chdir(row['SOI_SFTP_FOLDER']):
-		files = sftp.listdir()
+	sftp.chdir(row['SOI_SFTP_FOLDER'])
+	files = sftp.listdir()
 
 	#print(files)
 
